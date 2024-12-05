f8e03df4715eb12dcf1fc05ddc2f6ef6ec29c3cc
adri-er <adriana.er98@hotmail.com>
2024-12-04 21:14:37 -0500

    fix addition method

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 def main():
     number_1 = input("Specify first number: ")
     number_2 = input("Specify second number: ")
+    return number_1 + number_2
 
 if __name__ == '__main__':
     main()
