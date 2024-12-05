6e5927e95b900bd546b8bf7bfaaed781b69f545c
adri-er <adriana.er98@hotmail.com>
2024-12-04 21:29:57 -0500

    return proper value

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 def main():
     number_1 = input("Specify first number: ")
     number_2 = input("Specify second number: ")
-    return number_1 + number_2
+    return multiply(number_1, number_2)
 
 
 def multiply(a, b):
